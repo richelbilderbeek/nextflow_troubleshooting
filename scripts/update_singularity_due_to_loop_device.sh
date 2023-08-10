df5dcc91c08b29979e30dd3d940110fc19e6c569
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-10 12:01:05 +0200

    Install

--- scripts/update_singularity_due_to_loop_device.sh
+++ scripts/update_singularity_due_to_loop_device.sh
@@ -7,4 +7,14 @@ singularity_version=$(singularity --version)
 
 echo "singularity_version: ${singularity_version}"
 
-# https://github.com/sylabs/singularity/releases/download/v3.11.4/singularity-ce_3.11.4-jammy_amd64.deb
+if echo "${singularity_version}" | grep -iq "3.11"
+then
+  echo "Singularity version 3.11 found. Bad!"
+else
+  echo "Singularity version 3.11 not found. That means we this is done!"
+  exit 0
+fi
+
+wget https://github.com/sylabs/singularity/releases/download/v3.11.4/singularity-ce_3.11.4-jammy_amd64.deb
+sudo dpkg --install singularity-ce_3.11.4-jammy_amd64.deb
+
