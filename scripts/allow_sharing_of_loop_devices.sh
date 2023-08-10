2a279d8e5fb9dc9bda63a9cdea5ebcc603208f7d
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-10 09:50:41 +0200

    Add script

---
+++ scripts/allow_sharing_of_loop_devices.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Modify /etc/singularity/singularity.conf
+# to allow the sharing of loop devices.
+#
+# Usage:
+#
+#   ./allow_sharing_of_loop_devices.sh
+#   ./scripts/allow_sharing_of_loop_devices.sh
+#
