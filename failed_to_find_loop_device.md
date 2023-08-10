381a57a4addff5e3718ceafaede6f1613e1219eb
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-10 11:04:47 +0200

    Increase number of loop devices

--- failed_to_find_loop_device.md
+++ failed_to_find_loop_device.md
@@ -6,12 +6,19 @@ FATAL:   container creation failed: mount /proc/self/fd/3->/var/lib/singularity/
 
 The solution (from [here](https://github.com/sylabs/singularity/issues/1499#issuecomment-1493282259))
 is to edit [/etc/singularity/singularity.conf](https://docs.sylabs.io/guides/latest/admin-guide/configfiles.html#singularity-conf) 
-and allow the sharing of loop devices.
+and 
 
-Or run [this script](scripts/allow_sharing_of_loop_devices.sh):
+  1. allow the sharing of loop devices
+  2. increase the number of loop devices
+
+This can be done by 
+
+  1. run [this script](scripts/allow_sharing_of_loop_devices.sh) 
+  2. run [this script](scripts/increase_number_of_loop_devices.sh)
 
 ```
-./scripts/allow_sharing_of_loop_devices.sh
+sudo ./scripts/allow_sharing_of_loop_devices.sh
+sudo ./scripts/increase_number_of_loop_devices.sh
 ```
 
 
--- scripts/increase_number_of_loop_devices.sh
+++ scripts/increase_number_of_loop_devices.sh
@@ -9,18 +9,18 @@
 #   sudo ./scripts/allow_sharing_of_loop_devices.sh
 #
 filename="/etc/singularity/singularity.conf"
-backup_filename="/etc/singularity/singularity.conf.bak"
+backup_filename="/etc/singularity/singularity.conf.bak_2"
 
 # Check before
 
 # Style of if statement from https://www.shellcheck.net/wiki/SC2143
-if grep -q "shared loop devices = no" ${filename}
+if grep -q "max loop devices = 256$" ${filename}
 then 
-  echo "No shared loop devices allowed"
+  echo "Only 256 loop devices allowed"
   echo "Making copy to ${backup_filename}"
   sudo cp ${filename} ${backup_filename}
 else 
-  echo "Shared loop devices allowed. Nothing to do :-)"
+  echo "More loop devices allowed. Nothing to do :-)"
   exit 0
 fi
 
@@ -29,14 +29,13 @@ if [ ! -f $backup_filename ]; then
   exit 42
 fi
 
-sed -i "s/shared loop devices = no/shared loop devices = yes/" "${filename}"
+sed -i "s/max loop devices = 256/max loop devices = 2560/" "${filename}"
 
-# Check after
-if grep -q "shared loop devices = no" ${filename}
+if grep -q "max loop devices = 256$" ${filename}
 then 
-  echo "No shared loop devices allowed"
-  echo "Making copy to ${backup_filename}"
-  sudo cp ${filename} ${backup_filename}
+  echo "Error: still only 256 loop devices allowed ...?"
+  exit 42
 else 
-  echo "Shared loop devices allowed"
+  echo "More loop devices allowed. Well done!"
+  exit 0
 fi
