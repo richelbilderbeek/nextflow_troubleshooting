c48f870445f8815b3d40c217876d7cac3e07eeeb
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-10 10:57:15 +0200

    Allow sharing of loop devices

--- scripts/allow_sharing_of_loop_devices.sh
+++ scripts/allow_sharing_of_loop_devices.sh
@@ -8,3 +8,34 @@
 #   ./allow_sharing_of_loop_devices.sh
 #   ./scripts/allow_sharing_of_loop_devices.sh
 #
+filename="/etc/singularity/singularity.conf"
+backup_filename="/etc/singularity/singularity.conf.bak"
+
+# Check before
+
+# Style of if statement from https://www.shellcheck.net/wiki/SC2143
+if grep -q "shared loop devices = no" ${filename}
+then 
+  echo "No shared loop devices allowed"
+  echo "Making copy to ${backup_filename}"
+  sudo cp ${filename} ${backup_filename}
+else 
+  echo "Shared loop devices allowed"
+fi
+
+if [ ! -f $backup_filename ]; then
+  echo "Backup file '${backup_filename}' not found"
+  exit 42
+fi
+
+sed -i "s/shared loop devices = no/shared loop devices = yes/" "${filename}"
+
+# Check after
+if grep -q "shared loop devices = no" ${filename}
+then 
+  echo "No shared loop devices allowed"
+  echo "Making copy to ${backup_filename}"
+  sudo cp ${filename} ${backup_filename}
+else 
+  echo "Shared loop devices allowed"
+fi
