d01fb27c8bc82438f23a7cf6de19246644affe29
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-18 10:21:08 +0200

    Improve scripts

--- scripts/update_singularity.sh
+++ scripts/update_singularity.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
 #
-# Update Singularity due to 
+# Update Singularity due to the newest version
 #
+# Usage:
+#
+#   ./scripts/update_singularity.sh
+#   ./scripts/update_singularity.sh -V
+#   ./scripts/update_singularity.sh --verbose
+#
+# Output:
+#
+#   * Prints 0 and has return code 0 when not
+#   * Prints 1 and has return code 1 when true
+#   * In verbose mode, prints more
+#
+verbose=$(("$#" != 0))
+newest_version="3.11.4"
 
-singularity_version=$(singularity --version)
-
-echo "singularity_version: ${singularity_version}"
-
-if echo "${singularity_version}" | grep -iq "3.11.1"
+if [ "${verbose}" -eq 1 ]
 then
-  echo "Singularity version 3.11.1 found. Bad!"
-else
-  echo "Singularity version 3.11.1 not found. That means we this is done!"
-  exit 0
+  echo "Verbose mode"
 fi
 
-wget --timestamping https://github.com/sylabs/singularity/releases/download/v3.11.4/singularity-ce_3.11.4-jammy_amd64.deb
-sudo dpkg --install singularity-ce_3.11.4-jammy_amd64.deb
-
-echo "New singularity_version: ${singularity_version}"
-if echo "${singularity_version}" | grep -iq "3.11.1"
+if [ ! -f scripts/has_newest_singularity.sh ]
 then
-  echo "Still found Singularity version 3.11.1 found. Bad!"
+  echo "ERROR: 'scripts/has_newest_singularity.sh' not found."
   exit 42
-else
-  echo "Singularity version 3.11.1 not found. That means it worked :-)"
+fi
+
+has_newest_singularity=$(bash scripts/has_newest_singularity.sh)
+if [[ ${has_newest_singularity} == "1" ]]
+then
+  echo "Singularity already in its latest version. Doing nothing :-)"
   exit 0
 fi
 
+
+if [ "${verbose}" -eq 1 ]
+then
+  echo "Updating to newer version of Singularity"
+fi
+
+wget --timestamping "https://github.com/sylabs/singularity/releases/download/v${newest_version}/singularity-ce_${newest_version}-jammy_amd64.deb"
+sudo dpkg --install "singularity-ce_${newest_version}-jammy_amd64.deb"
+
+has_newest_singularity=$(bash scripts/has_newest_singularity.sh)
+if [[ ${has_newest_singularity} == "1" ]]
+then
+  echo "Success: Singularity now in its latest version"
+  exit 0
+else
+  echo "ERROR: Singularity still in an older version...?"
+  exit 1
+fi
