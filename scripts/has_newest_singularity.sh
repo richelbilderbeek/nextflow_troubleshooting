7a9a6d32b3249c1b5643b6440c6794de57ba1e79
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-18 09:59:07 +0200

    Detect newest Singularity

---
+++ scripts/has_newest_singularity.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Determine if Singularity is in the newest version
+#
+# Usage:
+#
+#   ./scripts/has_newest_singularity.sh
+#   ./scripts/has_newest_singularity.sh -V
+#   ./scripts/has_newest_singularity.sh --verbose
+#
+# Output:
+#
+#   * Prints 0 and has return code 0 when not
+#   * Prints 1 and has return code 1 when true
+#   * In verbose mode, prints more
+#
+verbose=$(("$#" != 0))
+newest_version="3.11.4"
+
+if [ "${verbose}" -eq 1 ]
+then
+  echo "Verbose mode"
+fi
+
+singularity_version=$(singularity --version)
+
+if [ "${verbose}" -eq 1 ]
+then 
+  echo "Newest singularity_version: ${newest_version}"
+  echo "Installed singularity_version: ${singularity_version}"
+fi
+
+
+if echo "${singularity_version}" | grep -iq "${newest_version}"
+then
+  if [ "${verbose}" -eq 1 ]
+  then 
+    echo "Latest Singularity version (${newest_version}) found."
+  fi
+  echo "1"
+  exit 1
+else
+  if [ "${verbose}" -eq 1 ]
+  then 
+    echo "Older version of Singularity found:"
+    echo "Newest singularity_version: ${newest_version}"
+    echo "Installed singularity_version: ${singularity_version}"
+    echo " "
+    echo "Tip: Run:"
+    echo " "
+    echo "  ./scripts/update_singularity.sh"
+  fi
+  echo "0"
+  exit 0
+fi
